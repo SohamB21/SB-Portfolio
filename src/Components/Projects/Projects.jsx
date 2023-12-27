332e3b05c25d5eb4f15d3046adf85914cfa2ccc2
Soham Banik <soham0110banik@gmail.com>
2023-12-28 03:18:22 +0530

    [feat,style]: languages added to project items,enhanced style

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TopCornImage from "../../assets/images/projects/TopCornImage.png";
 import NoteNestImage from "../../assets/images/projects/NoteNestImage.jpg";
@@ -12,7 +12,8 @@ const projectData = [
     imageSrc: TopCornImage,
     altText: "TopCorn",
     description:
-      "TopCorn is a React Native movie app for enthusiasts, featuring a vast library, detailed movie info, and easy ticket booking. Developed cross-platform features prioritizing user-friendly interfaces, encrypted storage, and performance enhancements based on user feedback.",
+      "A movie app for enthusiasts, featuring a vast library, detailed movie info and easy ticket booking. Developed cross-platform features prioritizing user-friendly design, encrypted storage and enhanced performance based on user feedback.",
+    languages: ["React Native", "TypeScript", "App Development"],
     deployedLink: null,
     githubLink: "https://github.com/SohamB21/TopCorn-MovieApp",
   },
@@ -20,91 +21,77 @@ const projectData = [
     imageSrc: NoteNestImage,
     altText: "NoteNest",
     description:
-      "NoteNest, a productivity-boosting note-taking platform, allows you to effortlessly create, edit, and organize your notes in a dynamic space. The intuitive web app facilitates real-time CRUD operations for note management, utilizing a secure MySQL database.",
+      "A productivity-boosting note-taking platform, allows you to effortlessly create, edit and organize your notes in a dynamic space. The intuitive web app facilitates real-time CRUD operations for note management, utilizing a secure database.",
+    languages: ["PHP", "MySQL", "JavaScript", "Tailwind CSS"],
     deployedLink: null,
     githubLink: "https://github.com/SohamB21/NoteNest",
   },
   {
     imageSrc: SportsQuoteGeneratorImage,
     altText: "Sports Quote Generator",
-    description: "This is a demo description.",
+    description:
+      "Showcases my journey from learning a new skill to creating something inspiring. This responsive tool lets you generate sports quotes from legendary athletes, easily copy and share your favorites on X/Twitter.",
+    languages: ["ReactJS", "Tailwind CSS"],
     deployedLink: "https://sports-quote-generator-react-js.vercel.app/",
     githubLink: "https://github.com/SohamB21/Sports-Quote-Generator-ReactJS",
   },
   {
     imageSrc: TypingSpeedTestGameImage,
     altText: "Typing Speed-Test Game",
-    description: "This is a demo description.",
+    description:
+      "This is a practical tool which can be used as a game to test someone's typing speed, on the basis of typing accuracy and words typed per minute (WPM) calculations.",
+    languages: ["JavaScript", "HTML", "CSS"],
     deployedLink: "https://sohamb21.github.io/Typing-SpeedTest-Game/",
     githubLink: "https://github.com/SohamB21/Typing-SpeedTest-Game",
   },
   {
     imageSrc: ResumeGeneratorImage,
     altText: "Resume Generator",
-    description: "This is a demo description.",
+    description:
+      "A user-friendly resume generator featuring a clean 2-column template, bullet fields, an extensive form and default picture feature. This responsive tool highlights skills for enhanced readability. Download resumes in PDF format with ease.",
+    languages: ["JavaScript", "HTML", "CSS", "Bootstrap"],
     deployedLink: "https://sohamb21.github.io/Resume-Generator/",
     githubLink: "https://github.com/SohamB21/Resume-Generator",
   },
   {
     imageSrc: StickyNotesImage,
     altText: "Sticky Notes",
-    description: "This is a demo description.",
-    deployedLink: null,
-    githubLink: null,
+    description:
+      "In place of Sticky Notes, I will show my portfolio as a project after I complete it totally.",
+    languages: ["JavaScript", "HTML", "CSS"],
+    deployedLink: "https://soham-banik-portfolio.vercel.app/",
+    githubLink: "https://github.com/SohamB21/SB-Portfolio",
   },
 ];
 
 const generateProjectItem = (project) => (
-  /*<div className="project relative group my-8 mx-10">
-    <img
-      src={project.imageSrc}
-      alt={project.altText}
-      className="w-96 h-56 shadow-xl shadow-blue-500 rounded-lg transition-transform transform group-hover:scale-105 filter group-hover:blur-sm"
-    />
-    <div
-      className={`info absolute inset-0 hidden group-hover:flex flex-col items-center justify-center text-center text-${project.textColor}`}
-    >
-      <h3 className="text-2xl font-bold m-2">{project.altText}</h3>
-      <p>A brief description of the project.</p>
-      <div className="flex flex-row items-center justify-center gap-6 mt-2">
-        {project.deployedLink && (
-          <a
-            href={project.deployedLink}
-            className="bg-blue-600 border border-blue-900 rounded-lg py-2 px-4"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deployed Project
-          </a>
-        )}
-        {project.githubLink && (
-          <a
-            href={project.githubLink}
-            className="bg-blue-600 border border-blue-900 rounded-lg py-2 px-4"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        )}
-      </div>
-    </div>
-  </div>*/
-  <div className="relative group overflow-hidden m-4 p-4 shadow-lg shadow-blue-500 rounded-lg border border-blue-200 flex flex-col justify-center items-center">
+  <div className="relative group overflow-hidden m-4 py-2 px-2 shadow-lg shadow-blue-500 rounded-lg border border-blue-200 flex flex-col justify-center items-center self-stretch">
     <img
       src={project.imageSrc}
       alt={project.altText}
       className="w-96 h-56 shadow-md shadow-blue-500 rounded-lg transition-transform hover:scale-105"
     />
-    <div className="info max-w-md flex flex-col items-center justify-center p-2 text-center text-indigo-950">
-      <h3 className="text-2xl font-bold m-2">{project.altText}</h3>
-      <p>{project.description}</p>
+
+    <div className="info max-w-md flex flex-col items-center justify-center p-1 text-center">
+      <h3 className="text-2xl font-bold mt-2">{project.altText}</h3>
+      <p className="text-sm">{project.description}</p>
+      <div className="flex flex-wrap gap-2 mt-2 justify-center">
+        {project.languages.map((language, index) => (
+          <span
+            key={index}
+            className="text-sm border-2 border-indigo-600 p-1 rounded-md hover:text-white hover:bg-indigo-600 hover:border-2 hover:border-blue-900 duration-300"
+          >
+            {language}
+          </span>
+        ))}
+      </div>
     </div>
-    <div className="flex flex-row items-center justify-center gap-4 text-white">
+
+    <div className="flex flex-row items-center justify-center gap-4 text-white mt-2">
       {project.deployedLink && (
         <a
           href={project.deployedLink}
-          className="bg-blue-600 border border-blue-900 rounded-lg py-2 px-4 m-2"
+          className="bg-blue-600 border border-blue-900 rounded-md py-2 px-4"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -114,7 +101,7 @@ const generateProjectItem = (project) => (
       {project.githubLink && (
         <a
           href={project.githubLink}
-          className="bg-blue-600 border border-blue-900 rounded-lg py-2 px-4 m-2"
+          className="bg-blue-600 border border-blue-900 rounded-md py-2 px-4"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -125,16 +112,40 @@ const generateProjectItem = (project) => (
   </div>
 );
 
-const Projects = () => (
-  <section
-    className="s h-full w-full flex flex-col items-center justify-center py-20"
-    style={{ background: "linear-gradient(to right, white, lightblue)" }}
-  >
-    <h2 className="text-3xl font-bold">My Projects</h2>
-    <div className="flex flex-wrap py-10 items-center justify-center">
-      {projectData.map(generateProjectItem)}
-    </div>
-  </section>
-);
+const Projects = () => {
+  const [showAllProjects, setShowAllProjects] = useState(false);
+  const visibleProjects = showAllProjects
+    ? projectData
+    : projectData.slice(0, 4);
+
+  return (
+    <section
+      className="s h-full w-full flex flex-col items-center justify-center py-20"
+      style={{ background: "linear-gradient(to right, white, lightblue)" }}
+    >
+      <h2 className="text-3xl font-bold">My Projects</h2>
+      <div className="flex flex-wrap pt-10 pb-4 items-center justify-center">
+        {visibleProjects.map(generateProjectItem)}
+      </div>
+
+      {!showAllProjects && (
+        <button
+          className="bg-indigo-600 border border-blue-900 rounded-md py-2 px-4 text-white"
+          onClick={() => setShowAllProjects(true)}
+        >
+          See More
+        </button>
+      )}
+      {showAllProjects && (
+        <button
+          className="bg-indigo-600 border border-blue-900 rounded-md py-2 px-4 text-white"
+          onClick={() => setShowAllProjects(false)}
+        >
+          See Less
+        </button>
+      )}
+    </section>
+  );
+};
 
 export default Projects;
