30bfbdd7f311261e4f398b06392c3f7d5844ebdc
Soham Banik <soham0110banik@gmail.com>
2024-01-10 02:27:40 +0530

    [style]: img for small screens

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -20,6 +20,23 @@ const Home = () => {
           <h1 className="lg:text-6xl md:text-6xl text-5xl font-semibold i-name">
             Soham Banik
           </h1>
+
+          <div className="lg:hidden md:hidden flex-1 i-right relative items-center justify-center flex">
+            <div className="h-full w-4/5 absolute flex flex-col items-center justify-center max-h-full relative">
+              <img
+                src={myphoto}
+                alt="My Photo"
+                className="i-img rounded-full shadow-md transition-transform transform hover:scale-110 duration-500"
+              />
+              <p
+                style={{ fontFamily: "Whisper" }}
+                className="text-4xl font-bold text-black"
+              >
+                Soham Banik
+              </p>
+            </div>
+          </div>
+
           <div className="i-title h-12 overflow-hidden lg:block md:block hidden">
             <div className="h-full i-title-wrapper">
               <div className="h-full i-title-item text-4xl font-bold text-green-700 flex items-center animate-move-delay-1">
@@ -97,7 +114,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="flex-1 i-right relative m-5 lg:mt-16 md:mt-16 mt-0">
+      <div className="lg:block md:block hidden flex-1 i-right relative m-5 lg:mt-16 md:mt-16 mt-0">
         <div className="i-bg h-full w-full absolute flex flex-col items-center justify-center max-h-full relative">
           <img
             src={myphoto}
