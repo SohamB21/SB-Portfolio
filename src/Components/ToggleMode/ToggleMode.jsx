58eed6180e95297dbda2526e08886f5aaad3921c
Soham Banik <soham0110banik@gmail.com>
2024-01-10 16:40:24 +0530

    [refactor]: toggle btn style and animation check

--- src/Components/ToggleMode/ToggleMode.jsx
+++ src/Components/ToggleMode/ToggleMode.jsx
@@ -20,18 +20,11 @@ const DarkModeToggle = () => {
           toggleDarkMode();
           handleClick();
         }}
-        className={`flex items-center p-2 ${
-          isDarkMode ? "bg-[#1e3a8a]" : "bg-[lightblue]"
-        } rounded-full text-black opacity-90 shadow-sm shadow-gray-500`}
+        className={`flex items-center p-2 ${isDarkMode ? 'bg-[#1e3a8a]' : 'bg-[lightblue]'} rounded-full text-black opacity-90 shadow-sm shadow-gray-500`}
       >
-        {isDarkMode ? (
-          <FiMoon
-            size={24}
-            className="motion-safe:animate-pulse duration-1000"
-          />
-        ) : (
-          <FiSun size={24} style={{ animation: "spin 15s linear infinite" }} />
-        )}
+        {isDarkMode ? 
+          <FiMoon size={24} className="motion-safe:animate-pulse duration-2000" /> : 
+          <FiSun size={24} className="motion-safe:animate-spin duration-2000" />}
       </button>
     </div>
   );
